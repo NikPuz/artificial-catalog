e3166a563d094272654616838f1fa1bc43d18aec
Grombik <nikita00891@yandex.ru>
2022-01-25 14:28:42 +0300

    Базовая работа http сервера

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	defer func() {
@@ -8,5 +14,20 @@ func main() {
 			fmt.Println(r)
         }
 	}()
-	
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {fmt.Fprintf(w, "Привет Мир")}).Methods("GET")
+
+    configServer(router).ListenAndServe()
+}
+
+func configServer (router *mux.Router) *http.Server {
+	return 	&http.Server{
+        Handler:      router,
+        Addr:         ":8000",
+        WriteTimeout: 5 * time.Second,
+        ReadTimeout:  5 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+    }
 }
